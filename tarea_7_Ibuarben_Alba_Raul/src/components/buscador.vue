2089f27fdc85058933c3808439c75946a01eebcf
raulibuarben <raulibu@gmail.com>
2026-04-24 11:44:57 +0200

    Cuadro de busqueda añadido

--- tarea_7_Ibuarben_Alba_Raul/src/components/buscador.vue
+++ tarea_7_Ibuarben_Alba_Raul/src/components/buscador.vue
@@ -1,5 +1,29 @@
+<script setup>
+// Definimos el evento que enviará el nombre a App.vue
+const emit = defineEmits(['buscar']);
 
+// Función que se activa cada vez que el usuario escribe
+const alEscribir = (evento) => {
+  emit('buscar', evento.target.value);
+};
+</script>
 
 <template>
- 
+  <div class="buscador-box">
+    <input 
+      type="text" 
+      placeholder="Escribe un nombre (ej: Rick)..." 
+      @input="alEscribir"
+      class="input-busqueda"
+    />
+  </div>
 </template>
+
+<style scoped>
+.input-busqueda {
+  padding: 10px;
+  width: 300px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+</style>
